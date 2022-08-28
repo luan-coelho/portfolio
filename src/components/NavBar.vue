9927e3af9fa1c5a9febfc6a7eee9414ace11555e
Luan <luancoelho.dev@gmail.com>
2022-08-28 18:21:15 -0300

    Melhora na NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,35 +9,17 @@
         Luan Coêlho de Souza
       </h1>
 
-      <div class="flex order-2">
-        <a
-          href="https://drive.google.com/file/d/18pAjoQRqOmrrNkOOkQds9Jn-Kk2ajYVj/view?usp=sharing"
-          target="_blank"
-        >
-          <button
-            type="button"
-            class="hidden text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Baixar CV
-            <font-awesome-icon
-              class="ml-2"
-              icon="fa-solid fa-address-card"
-              size="xl"
-            />
-          </button>
-        </a>
-      </div>
       <div
-        class="justify-between items-center w-full flex md:w-auto md:order-1"
+        class="items-center justify-center w-full flex md:w-auto md:order-1"
         id="navbar-sticky"
       >
         <ul
-          class="flex flex-row items-center justify-center p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-root-color-dark dark:border-gray-700"
+          class="flex flex-row items-center justify-center sx:p-2 sm:p-3 md:p-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-root-color-dark dark:border-gray-700"
         >
           <li>
             <router-link
               to="/"
-              class="block py-2 pr-4 pl-3 rounded md:bg-transparen md:p-0 md:hover:text-blue-700 dark:text-white"
+              class="block py-2 pr-4 pl-3 rounded md:bg-transparen md:p-0 md:hover:text-blue-700 dark:text-white text-center"
               aria-current="page"
               >Sobre mim</router-link
             >
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,34 @@
 <template>
   <div
-    class="dark:bg-root-color-litledark flex justify-center items-center w-full h-screen"
+    class="dark:bg-root-color-litledark flex flex-col justify-center items-center w-full h-screen"
   >
-    <h1 class="dark:text-white text-4xl">Em construção</h1>
+    <h1 class="dark:text-white mb-5 text-4xl">Em construção</h1>
+    <div>
+      <a
+        href="https://drive.google.com/file/d/18pAjoQRqOmrrNkOOkQds9Jn-Kk2ajYVj/view?usp=sharing"
+        target="_blank"
+      >
+        <button
+          type="button"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Baixar CV
+          <font-awesome-icon
+            class="ml-2"
+            icon="fa-solid fa-address-card"
+            size="xl"
+          />
+        </button>
+      </a>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from "vue";
+  import { defineComponent } from 'vue';
 
   export default defineComponent({
-    name: "AboutView",
+    name: 'AboutView',
     components: {},
     data() {
       return {};
